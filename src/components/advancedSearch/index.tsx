4586c875f2017d41c5ecacaa8dc677045b6cff97
Athma Vasi <athma.vasi@protonmail.com>
2023-01-23 17:51:10 -0700

    make `find results` section of advancedSearch form responsive

--- src/components/advancedSearch/index.tsx
+++ src/components/advancedSearch/index.tsx
@@ -23,7 +23,7 @@ function AdvancedSearch({
     <div>
       <form action="#" method="GET">
         {/* search term specificity modifiers */}
-        <Grid columns={width < 576 ? 1 : 4} style={{ outline: "2px solid GrayText" }}>
+        <Grid columns={width < 576 ? 1 : 4} p={width < 576 ? "sm" : "md"}>
           <Grid.Col span={1}>
             <Center style={{ width: "100%", height: "100%" }}>
               <Text>Find results</Text>
@@ -31,55 +31,83 @@ function AdvancedSearch({
           </Grid.Col>
 
           {/* contains label and inputs */}
-          <Grid.Col span={width < 576 ? 1 : 2}>
-            <Flex gap="sm" direction="column" justify="space-evenly" align="center">
-              <Grid columns={2} align="space-between">
+          <Grid.Col span={width < 576 ? 1 : 2} style={{ outline: "2px solid GrayText" }}>
+            <Flex
+              gap="sm"
+              direction="column"
+              justify="space-evenly"
+              align="stretch"
+              mx={width < 576 ? "sm" : "md"}
+            >
+              <Grid
+                columns={width < 576 ? 1 : 2}
+                style={{ outline: "2px solid GrayText" }}
+                py={width < 576 ? "sm" : "md"}
+              >
                 <Grid.Col span={1}>
                   <Text>
                     containing <strong>all</strong> of the words
                   </Text>
                 </Grid.Col>
                 <Grid.Col span={1}>
-                  <TextInput />
+                  <TextInput size="lg" />
                 </Grid.Col>
               </Grid>
-              <Grid columns={2} align="space-between">
+              <Grid
+                columns={width < 576 ? 1 : 2}
+                style={{ outline: "2px solid GrayText" }}
+                py={width < 576 ? "sm" : "md"}
+              >
                 <Grid.Col span={1}>
                   <Text>
                     containing the <strong>exact phrase</strong>
                   </Text>
                 </Grid.Col>
                 <Grid.Col span={1}>
-                  <TextInput />
+                  <TextInput size="lg" />
                 </Grid.Col>
               </Grid>
-              <Grid columns={2} align="space-between">
+              <Grid
+                columns={width < 576 ? 1 : 2}
+                style={{ outline: "2px solid GrayText" }}
+                py={width < 576 ? "sm" : "md"}
+              >
                 <Grid.Col span={1}>
                   <Text>
                     containing <strong>at least one</strong> of the words
                   </Text>
                 </Grid.Col>
                 <Grid.Col span={1}>
-                  <TextInput />
+                  <TextInput size="lg" />
                 </Grid.Col>
               </Grid>
-              <Grid columns={2} align="space-between">
+              <Grid
+                columns={width < 576 ? 1 : 2}
+                style={{ outline: "2px solid GrayText" }}
+                py={width < 576 ? "sm" : "md"}
+              >
                 <Grid.Col span={1}>
                   <Text>
                     containing <strong>none</strong> of the words
                   </Text>
                 </Grid.Col>
                 <Grid.Col span={1}>
-                  <TextInput />
+                  <TextInput size="lg" />
                 </Grid.Col>
               </Grid>
             </Flex>
           </Grid.Col>
           {/* search results amount modifier */}
           <Grid.Col span={1}>
-            <Center style={{ width: "100%", height: "100%" }}>
-              <NativeSelect data={["10", "20", "30", "40"]} label="Results per page" />
-            </Center>
+            {width > 576 ? (
+              <Center style={{ width: "100%", height: "100%" }}>
+                <NativeSelect data={["10", "20", "30", "40"]} label="Results per page" />
+              </Center>
+            ) : (
+              <Flex justify="flex-start" align="center" mx={width < 576 ? "sm" : "md"}>
+                <NativeSelect data={["10", "20", "30", "40"]} label="Results per page" />
+              </Flex>
+            )}
           </Grid.Col>
         </Grid>
         {/* search filters: partial, full, e-books(paid, full)*/}
