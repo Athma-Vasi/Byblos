0ac71e9282cf4049e439d8a0c926ce71a2a402eb
Athma Vasi <athma.vasi@protonmail.com>
2023-02-05 20:58:39 -0700

    save point before adding infinite scroll, added github links to footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,11 +1,21 @@
-import { Footer } from "@mantine/core";
+import { Flex, Footer, Space, Text } from "@mantine/core";
 
 type MyFooterProps = { children?: React.ReactNode };
 
 function MyFooter({ children }: MyFooterProps) {
   return (
     <Footer height={60} p="md">
-      Application footer
+      <Flex direction="row" justify="center" align="center">
+        <Text>
+          Made by <a href="https://github.com/Athma-Vasi">Athma Vasi</a>
+        </Text>
+
+        <Space w="xl" />
+
+        <Text>
+          <a href="https://github.com/Athma-Vasi/Byblos">View code</a>
+        </Text>
+      </Flex>
     </Footer>
   );
 }
